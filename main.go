2b15d576263b9055e4492402ed153a2943e0e77b
Watchapon Jun-o-pat <watchapon@igeargeek.com>
2021-07-16 18:14:27 +0700

    [Edited] - asd

--- main.go
+++ main.go
@@ -1,45 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/lucifez385/golang-line-notify/router"
 )
 
-type addressBook struct {
-	Firstname string
-	Lastname  string
-	Code      int
-	Phone     string
-}
-
-func getAddressBookAll(w http.ResponseWriter, r *http.Request) {
-	addBook := addressBook{
-		Firstname: "Chaiyarin",
-		Lastname:  "Niamsuwan",
-		Code:      1993,
-		Phone:     "0870940955",
-	}
-	json.NewEncoder(w).Encode(addBook)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the HomePage!")
-}
-func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/getAddress", getAddressBookAll)
-	http.ListenAndServe(getPort(), nil)
-}
-func getPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
-	return ":" + port
-}
 func main() {
-	handleRequest() // ต้องนำ handleRequest มาใส่ใน main ด้วยนะครับ
+	app := fiber.New()
+
+	app.Get("/healthz", func(c *fiber.Ctx) error {
+		return c.SendString("Server is running 👋!")
+	})
+
+	router.V1(app)
+	app.Listen(":8080")
+	fmt.Println("Server is running")
 }
